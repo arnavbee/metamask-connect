5f90600fc7cd57644aa687d5edce96bd9bd49ea2
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-05-21 16:09:02 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
-import {Contract, providers} from "ethers";
+import { BrowserProvider, parseUnits } from "ethers";
+import { ethers } from 'ethers';
+
 
 function App() {
 
@@ -13,11 +15,39 @@ function App() {
     }
   }, []);
 
-  return (
-    <>
-     
-    </>
-  )
+  async function connectWallet(){
+    window.ethereum
+    .request({
+      method: "eth_requestAccounts"
+    })
+    .then((accounts) => {
+      setAccount(accounts[0]);
+    })
+    .catch((error) => {
+      alert("Something went wrong")
+    });
+  }
+
+  if (account === null) {
+    return (
+      <div className="App">
+        {
+          isWalletInstalled ? (
+            <button onClick={connectWallet}>Connect Wallet</button>
+          ) : (
+            <p>Install Metamask wallet</p>
+          )
+        }
+
+      </div>
+    );
+  }
+    return (
+      <div className="App">
+        <p>Connected as: {account}</p>
+      </div>
+  );
 }
+
 
 export default App
